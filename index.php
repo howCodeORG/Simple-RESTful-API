141e1dc29f24582dc9fc29f7810153b2373b38df
Francis McNamee <francis@reedoo.org>
2017-04-02 02:15:17 +0100

    Added source code
    Added source code

--- index.php
+++ index.php
@@ -4,9 +4,38 @@ require_once("DB.php");
 $db = new DB("127.0.0.1", "SocialNetwork", "root", "");
 
 if ($_SERVER['REQUEST_METHOD'] == "GET") {
-        echo json_encode($db->query("SELECT * FROM users"));
+
+        if ($_GET['url'] == "auth") {
+
+        } else if ($_GET['url'] == "users") {
+
+        }
+
 } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        echo "POST";
+
+        if ($_GET['url'] == "auth") {
+                $postBody = file_get_contents("php://input");
+                $postBody = json_decode($postBody);
+
+                $username = $postBody->username;
+                $password = $postBody->password;
+
+                if ($db->query('SELECT username FROM users WHERE username=:username', array(':username'=>$username))) {
+                        if (password_verify($password, $db->query('SELECT password FROM users WHERE username=:username', array(':username'=>$username))[0]['password'])) {
+                                $cstrong = True;
+                                $token = bin2hex(openssl_random_pseudo_bytes(64, $cstrong));
+                                $user_id = $db->query('SELECT id FROM users WHERE username=:username', array(':username'=>$username))[0]['id'];
+                                $db->query('INSERT INTO login_tokens VALUES (\'\', :token, :user_id)', array(':token'=>sha1($token), ':user_id'=>$user_id));
+                                echo '{ "Token": "'.$token.'" }';
+                        } else {
+                                http_response_code(401);
+                        }
+                } else {
+                        http_response_code(401);
+                }
+
+        }
+
 } else {
         http_response_code(405);
 }
